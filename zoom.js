a20a406be0fff79e7f4c20841ed3724c875bae20
Famp <the.art.of.famp@gmail.com>
2022-01-17 00:51:41 -0300

    testing_zoom_feature
    testando dynamic zoom.

--- zoom.js
+++ zoom.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-    var tela_h = window.screen.height;
-    var tela_w = window.screen.width;
     
-    console.log(tela_h, tela_w);
+    var scale = DynamicZoom();
+    
+    document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+    document.body.style.msTransform =   scale;       // IE 9
+    document.body.style.transform = scale;     // Geral
+
+    console.log(scale);
 })
+
+function DynamicZoom (){
+    
+    if(window.screen.width < 800)
+        return ((window.screen.width * 100)/800);
+    
+    else
+        return 1.0;
+}
